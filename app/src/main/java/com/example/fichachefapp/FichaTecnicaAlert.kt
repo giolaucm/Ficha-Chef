748a3cd6f4688e0be969563b41b74988eaa74404
Giovanna Carvalho <82350376+Giolau12@users.noreply.github.com>
2023-11-08 22:53:22 -0300

    Desenvolvimento front das telas de "Recuperar senha" e dando início a tela da ficha técnica.

---
+++ app/src/main/java/com/example/fichachefapp/FichaTecnicaAlert.kt
@@ -0,0 +1,11 @@
+package com.example.fichachefapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FichaTecnicaAlert : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ficha_tecnica_alert)
+    }
+}
---
+++ app/src/main/java/com/example/fichachefapp/Senha.kt
@@ -0,0 +1,11 @@
+package com.example.fichachefapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Senha : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_senha)
+    }
+}
